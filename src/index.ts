87bdc6f407b6a42fcea617664d0db7f483bee818
Juan Francisco <contrerasjf.0@hotmail.com>
2020-04-13 22:18:57 -0500

    Definition of an observable
    Resolve: S03-C13

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
+import { Observable } from 'rxjs';
 
 
-console.log('Hola Mundo!');
+//const obs$ = Observable.create();
 
+const obs$ =  new Observable<string>((subs) => {
+    subs.next('Text 1');
+    subs.next('Text 2');
 
+    subs.complete();
 
+    subs.next('Text 3');
+    subs.next('Text 4');
+});
 
-
-
+obs$.subscribe(console.log);
 

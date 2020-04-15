715567b124aa86672c23ebf86f123538997b1995
Juan Francisco <contrerasjf.0@hotmail.com>
2020-04-15 02:31:52 -0500

    Other ways to define the subscribe and use the other arguments
    Resolve: S02-C14

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
+const observer: Observer<any> = {
+    next: value => console.log('next: ', value),
+    error: error => console.warn('error: ', error),
+    complete: () => console.info('completed')
+}
 
 //const obs$ = Observable.create();
 
@@ -7,11 +12,22 @@ const obs$ =  new Observable<string>((subs) => {
     subs.next('Text 1');
     subs.next('Text 2');
 
+
+    // forced an error
+
+    /* const person = undefined;
+    person.name = 'Luis'; */
+
     subs.complete();
 
     subs.next('Text 3');
     subs.next('Text 4');
 });
 
-obs$.subscribe(console.log);
+/* obs$.subscribe( 
+    value => console.log('next: ', value),
+    error => console.warn('error: ', error),
+    () => console.info('completed')
+ ); */
+ obs$.subscribe(observer);
 

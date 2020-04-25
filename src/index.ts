0e7f840a79d48c4fb882e9127622ddbed0fb5328
Juan Francisco <contrerasjf.0@hotmail.com>
2020-04-25 00:51:41 -0500

    Handle of subscription and unsubscribe
    Resolve: S03-C16

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,32 @@ import { Observable, Observer } from 'rxjs';
 const observer: Observer<any> = {
     next: value => console.log('next: ', value),
     error: error => console.warn('error: ', error),
-    complete: () => console.info('completed')
+    complete: () => console.info('completed') 
 }
+
+const interval$ = new Observable<number>( subscriber => {
+    let counter = 0;
+
+    const intervalPID = setInterval(()=>{
+        subscriber.next(counter++);
+        console.log(counter);
+    }, 1000);
+
+    return () => {
+        clearInterval(intervalPID);
+        console.log('The interval has been destroyed');
+    }
+
+});
+
+const subs1= interval$.subscribe(num => console.log(num));
+const subs2= interval$.subscribe(num => console.log(num));
+const subs3= interval$.subscribe(num => console.log(num));
+
+setTimeout(() => {
+    subs1.unsubscribe();
+    subs2.unsubscribe();
+    subs3.unsubscribe();
+
+    console.log('Completed timeout');
+}, 3000);
